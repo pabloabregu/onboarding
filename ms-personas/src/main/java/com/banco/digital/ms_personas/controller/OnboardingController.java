554fcb078e6db5d160e2729792290fa587e6bd48
pabloabregu <pablo.abregu@hotmail.com>
2024-08-17 01:02:29 -0300

    Refactorizacion del servicio

--- ms-personas/src/main/java/com/banco/digital/ms_personas/controller/OnboardingController.java
+++ ms-personas/src/main/java/com/banco/digital/ms_personas/controller/OnboardingController.java
@@ -2,7 +2,7 @@ package com.banco.digital.ms_personas.controller;
 
 import com.banco.digital.ms_personas.request.UserRequest;
 import com.banco.digital.ms_personas.response.Response;
-import com.banco.digital.ms_personas.service.UserService;
+import com.banco.digital.ms_personas.service.OnboardingService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,20 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
-public class UserController {
+public class OnboardingController {
 
     @Autowired
-    private UserService userService;
+    private OnboardingService onboardingService;
 
-    /**
-     * Crea un nuevo usuario y realiza el alta de producto.
-     *
-     * @param userRequest Contiene los datos del usuario a crear.
-     * @return ResponseEntity con la respuesta de la operación.
-     */
-    @PostMapping("/createUser")
-    public ResponseEntity<Response> createUser(@RequestBody UserRequest userRequest) throws JsonProcessingException {
-        Response response = userService.createUser(userRequest);
+    @PostMapping("/register")
+    public ResponseEntity<Response> register(@RequestBody UserRequest userRequest) throws JsonProcessingException {
+        Response response = onboardingService.register(userRequest);
         return ResponseEntity.ok(response);
     }
 }
