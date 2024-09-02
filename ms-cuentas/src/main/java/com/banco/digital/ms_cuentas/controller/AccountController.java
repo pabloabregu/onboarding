9914460268d1d0c45394f0339f61cfd5c08aa4b6
pabloabregu <pablo.abregu@hotmail.com>
2024-09-01 23:44:26 -0300

    Mejoras del controller de cuentas

--- ms-cuentas/src/main/java/com/banco/digital/ms_cuentas/controller/AccountController.java
+++ ms-cuentas/src/main/java/com/banco/digital/ms_cuentas/controller/AccountController.java
@@ -1,13 +1,13 @@
 package com.banco.digital.ms_cuentas.controller;
 
-import com.banco.digital.ms_cuentas.request.Request;
 import com.banco.digital.ms_cuentas.request.UserAccountRequest;
 import com.banco.digital.ms_cuentas.response.Response;
+import com.banco.digital.ms_cuentas.service.AccountEventProcessorService;
 import com.banco.digital.ms_cuentas.service.AccountService;
 import com.banco.digital.ms_cuentas.service.AccountStatusService;
 import com.banco.digital.ms_cuentas.service.CurrencyCodeService;
-import com.banco.digital.ms_cuentas.service.AccountEventProcessorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,33 +15,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("/api/accounts")
 public class AccountController {
+    private final AccountEventProcessorService accountEventProcessorService;
 
     @Autowired
-    AccountService accountService;
-
-    @Autowired
-    CurrencyCodeService currencyCodeService;
-
-    @Autowired
-    AccountStatusService accountStatusService;
-
-    @Autowired
-    private AccountEventProcessorService accountEventProcessorService;
-
-    @PostMapping("/validateExternal")
-    public ResponseEntity<Response> externalValidation(@RequestBody Request request) throws URISyntaxException, IOException, InterruptedException {
-        return ResponseEntity.ok(accountService.externalValidation(request.getDni(), BigDecimal.valueOf(request.getSalary())));
+    public AccountController(AccountEventProcessorService accountEventProcessorService) {
+        this.accountEventProcessorService = accountEventProcessorService;
     }
 
     @PostMapping("/create")
-    public ResponseEntity create(@RequestBody UserAccountRequest userAccountRequest) throws URISyntaxException, IOException, InterruptedException {
+    public ResponseEntity<Response> create(@RequestBody UserAccountRequest userAccountRequest) throws URISyntaxException, IOException, InterruptedException {
         accountEventProcessorService.processAccountCreation(userAccountRequest);
-        return ResponseEntity.ok("CREATE");
+        return ResponseEntity.ok(new Response("CREATE!", HttpStatus.CREATED.value()));
     }
 }
