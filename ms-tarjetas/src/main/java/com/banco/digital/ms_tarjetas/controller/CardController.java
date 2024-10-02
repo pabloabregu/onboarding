cf6921015f78417d71a3bd72984a8cb361c9fae6
pabloabregu <pablo.abregu@hotmail.com>
2024-10-02 20:04:31 -0300

    Controller ms-tarjeta

--- ms-tarjetas/src/main/java/com/banco/digital/ms_tarjetas/controller/CardController.java
+++ ms-tarjetas/src/main/java/com/banco/digital/ms_tarjetas/controller/CardController.java
@@ -1,7 +1,9 @@
 package com.banco.digital.ms_tarjetas.controller;
 
 import com.banco.digital.ms_tarjetas.model.Card;
-import com.banco.digital.ms_tarjetas.request.IssueCardRequest;
+import com.banco.digital.ms_tarjetas.request.RegisterCardRequest;
+import com.banco.digital.ms_tarjetas.response.RegisterCardResponse;
+import com.banco.digital.ms_tarjetas.response.Response;
 import com.banco.digital.ms_tarjetas.service.CardEventProcessorService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,13 +24,9 @@ public class CardController {
         this.cardEventProcessorService = cardEventProcessorService;
     }
 
-    @PostMapping("/issue")
-    public ResponseEntity<String> issueCard(@Valid @RequestBody IssueCardRequest request) {
-        try {
-            Card card = cardEventProcessorService.processCardCreation(request);
-            return new ResponseEntity<>("Card issued successfully! " + card.toString(), HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>("An error occurred while issuing the card", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    @PostMapping("/register")
+    public ResponseEntity<Response> registerCard(@Valid @RequestBody RegisterCardRequest request) {
+        RegisterCardResponse response = cardEventProcessorService.processCardCreation(request);
+        return ResponseEntity.status(response.getHttpStatusCode()).body(response);
     }
 }
